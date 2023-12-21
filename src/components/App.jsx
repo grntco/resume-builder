ed37dee22c9b260f39917840237ffaedeb4f7653
Grant Collins <grantcollins03@gmail.com>
2023-12-21 15:35:35 -0500

    added another div main-container inside main-content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,50 +11,52 @@ export function App() {
     <div className="app">
       <Header />
       <section className="main-content">
-        <div className="cards-container">
-          <Card>
-            <div className="toolbar">
-              <button>Toggle Preview</button>
-              <button>Download as PDF</button>
-            </div>
-          </Card>
-          <Card title="Personal Details">
-            <Form>
-              <TextInput label="Full Name"></TextInput>
-              <TextInput label="Email"></TextInput>
-              <TextInput label="Phone Number"></TextInput>
-              <TextInput label="Location"></TextInput>
-            </Form>
-          </Card>
-          <Card title="Work Experience">
-            <Form>
-              <TextInput label="Company"></TextInput>
-              <TextInput label="Position"></TextInput>
-              <TextInput label="Location"></TextInput>
-              <TextInput label="Start Date"></TextInput>
-              <TextInput label="End Date"></TextInput>
-            </Form>
-            <button>Add Experience</button>
-          </Card>
-          <Card title="Education">
-            <Form>
-              <TextInput label="University"></TextInput>
-              <TextInput label="Degree"></TextInput>
-              <TextInput label="Location"></TextInput>
-              <TextInput label="Start Date"></TextInput>
-              <TextInput label="End Date"></TextInput>
-            </Form>
-            <button>Add Education</button>
-          </Card>
-          <Card title="Skills & Languages">
-            <Form>
-              <TextInput label="Skill"></TextInput>
-            </Form>
-            <button>Add Skill</button>
-          </Card>
-        </div>
-        <div className="preview-container">
-          <Resume></Resume>
+        <div className="main-container">
+          <div className="cards-container">
+            <Card>
+              <div className="toolbar">
+                <button>Toggle Preview</button>
+                <button>Download as PDF</button>
+              </div>
+            </Card>
+            <Card title="Personal Details">
+              <Form>
+                <TextInput label="Full Name"></TextInput>
+                <TextInput label="Email"></TextInput>
+                <TextInput label="Phone Number"></TextInput>
+                <TextInput label="Location"></TextInput>
+              </Form>
+            </Card>
+            <Card title="Work Experience">
+              <Form>
+                <TextInput label="Company"></TextInput>
+                <TextInput label="Position"></TextInput>
+                <TextInput label="Location"></TextInput>
+                <TextInput label="Start Date"></TextInput>
+                <TextInput label="End Date"></TextInput>
+              </Form>
+              <button>Add Experience</button>
+            </Card>
+            <Card title="Education">
+              <Form>
+                <TextInput label="University"></TextInput>
+                <TextInput label="Degree"></TextInput>
+                <TextInput label="Location"></TextInput>
+                <TextInput label="Start Date"></TextInput>
+                <TextInput label="End Date"></TextInput>
+              </Form>
+              <button>Add Education</button>
+            </Card>
+            <Card title="Skills & Languages">
+              <Form>
+                <TextInput label="Skill"></TextInput>
+              </Form>
+              <button>Add Skill</button>
+            </Card>
+          </div>
+          <div className="preview-container">
+            <Resume></Resume>
+          </div>     
         </div>
       </section>
       <Footer />
