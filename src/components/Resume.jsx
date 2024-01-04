f95a6f1215a501e5cfd6c33f5e02d508faf1e1d6
Grant Collins <grantcollins03@gmail.com>
2024-01-04 11:09:33 -0500

    styled skills section and fixed resume line-height

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -13,7 +13,38 @@ export function Resume() {
             </div>
             <div className="resume__section">
                 <h3 className="resume__section-title">Work Experience</h3>
-                {/* Experience Container will get repeated */}
+                <div className="resume__experience-container">
+                    <div className="resume__details-container">
+                        <h4>Amazon</h4>
+                        <p>Aug. 2020 - Jan. 2024</p>
+                    </div>
+                    <div className="resume__details-container">
+                        <p>Jr. Software Developer</p>
+                        <p>Remote</p>
+                    </div>
+                    <ul className="resume__responsibilities-container">
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat veniam natus fugit! Incidunt labore libero sapiente, tempore assumenda ut ea?</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                    </ul>
+                </div>
+                <div className="resume__experience-container">
+                    <div className="resume__details-container">
+                        <h4>Amazon</h4>
+                        <p>Aug. 2020 - Jan. 2024</p>
+                    </div>
+                    <div className="resume__details-container">
+                        <p>Jr. Software Developer</p>
+                        <p>Remote</p>
+                    </div>
+                    <ul className="resume__responsibilities-container">
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                    </ul>
+                </div>
                 <div className="resume__experience-container">
                     <div className="resume__details-container">
                         <h4>Amazon</h4>
@@ -33,35 +64,58 @@ export function Resume() {
             </div>
             <div className="resume__section">
                 <h3 className="resume__section-title">Education</h3>
-                {/* <div className="experience-container">
-                    <div>
-                        <h4 className="experience-institution">Greenville Technical College</h4>
-                        <p className="experience-dates">August 2017-May 2020</p>
+                <div className="resume__experience-container">
+                    <div className="resume__details-container">
+                        <h4>Greenville Technical College</h4>
+                        <p>Aug. 2017 - May 2020</p>
                     </div>
-                    <div className="position-container">
-                        <p className="position-title-degree">B.S. in Computer Science</p>
-                        <p className="position-location">Greenville, SC</p>
-                        <ul className="position-responsibilities">
-                            <li>Lorem ipsum dolor sit amet.</li>
-                            <li>Lorem ipsum dolor sit amet.</li>
-                            <li>Lorem ipsum dolor sit amet.</li>
-                            <li>Lorem ipsum dolor sit amet.</li>
-                        </ul>
+                    <div className="resume__details-container">
+                        <p>B.S. in Computer Science</p>
+                        <p>Greenville, SC</p>
                     </div>
-                </div> */}
+                    <ul className="resume__responsibilities-container">
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                    </ul>
+                </div>
+                <div className="resume__experience-container">
+                    <div className="resume__details-container">
+                        <h4>Greenville Technical College</h4>
+                        <p>Aug. 2017 - May 2020</p>
+                    </div>
+                    <div className="resume__details-container">
+                        <p>B.S. in Computer Science</p>
+                        <p>Greenville, SC</p>
+                    </div>
+                    <ul className="resume__responsibilities-container">
+                        <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum eum, alias possimus eius, natus suscipit inventore saepe rem doloremque assumenda optio aspernatur minima in illo.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                    </ul>
+                </div>
             </div>
             <div className="resume__section">
                 <h3 className="resume__section-title">Skills</h3>
-                <div className="skills-container">
-                    <ul>
-                        <li>HTML/CSS</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Git</li>
-                        <li>Webpack</li>
-                        <li>VS Code</li>
-                    </ul>
-                </div>
+                <ul className="resume__skills-container">
+                    <li className="resume__skill-item">HTML/CSS</li>
+                    <li className="resume__skill-item">JavaScript</li>
+                    <li className="resume__skill-item">React</li>
+                    <li className="resume__skill-item">Git</li>
+                    <li className="resume__skill-item">Webpack</li>
+                    <li className="resume__skill-item">VS Code</li>
+                    <li className="resume__skill-item">HTML/CSS</li>
+                    <li className="resume__skill-item">JavaScript</li>
+                    <li className="resume__skill-item">React</li>
+                    <li className="resume__skill-item">Git</li>
+                    <li className="resume__skill-item">Webpack</li>
+                    <li className="resume__skill-item">VS Code</li>
+                    <li className="resume__skill-item">Webpack</li>
+                    <li className="resume__skill-item">VS Code</li>
+                    <li className="resume__skill-item">HTML/CSS</li>
+                </ul>
             </div>
         </div>
     )
