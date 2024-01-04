b08e1d4cdebb3dcc6e55c5f569ac49e4c954b62c
Grant Collins <grantcollins03@gmail.com>
2024-01-04 10:35:57 -0500

    styled resume personal details and experience container

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,37 +3,37 @@ import '../styles/Resume.css'
 export function Resume() {
     return (
         <div className="resume">
-            <div className="resume__personal-details">
-                <h2>Grant Collins</h2>
-                <div className="contact-info-container">
-                    <div className="email">grantcollins03@gmail.com</div>
-                    <div className="phone-number">(803) 445-3607</div>
-                    <div className="location">Greenville, SC</div>
-                </div>
+            <div className="resume__section">
+                <h2 className="resume__name">Grant Collins</h2>
+                <ul className="resume__contact-info-container">
+                    <li className="email">grantcollins03@gmail.com</li>
+                    <li className="phone-number">(803) 445-3607</li>
+                    <li className="location">Greenville, SC</li>
+                </ul>
             </div>
-            <div className="work-container">
-                <h3>Work Experience</h3>
+            <div className="resume__section">
+                <h3 className="resume__section-title">Work Experience</h3>
                 {/* Experience Container will get repeated */}
-                <div className="experience-container">
-                    <div>
-                        <h4 className="experience-institution">Amazon</h4>
-                        <p className="experience-dates">August 2020-January 2024</p>
+                <div className="resume__experience-container">
+                    <div className="resume__details-container">
+                        <h4>Amazon</h4>
+                        <p>Aug. 2020 - Jan. 2024</p>
                     </div>
-                    <div className="position-container">
-                        <p className="position-title">Jr. Software Developer</p>
-                        <p className="position-location">Remote</p>
-                        <ul className="position-responsibilities">
-                            <li>Lorem ipsum dolor sit amet.</li>
-                            <li>Lorem ipsum dolor sit amet.</li>
-                            <li>Lorem ipsum dolor sit amet.</li>
-                            <li>Lorem ipsum dolor sit amet.</li>
-                        </ul>
+                    <div className="resume__details-container">
+                        <p>Jr. Software Developer</p>
+                        <p>Remote</p>
                     </div>
+                    <ul className="resume__responsibilities-container">
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                    </ul>
                 </div>
             </div>
-            <div className="education-container">
-                <h3>Education</h3>
-                <div className="experience-container">
+            <div className="resume__section">
+                <h3 className="resume__section-title">Education</h3>
+                {/* <div className="experience-container">
                     <div>
                         <h4 className="experience-institution">Greenville Technical College</h4>
                         <p className="experience-dates">August 2017-May 2020</p>
@@ -48,10 +48,10 @@ export function Resume() {
                             <li>Lorem ipsum dolor sit amet.</li>
                         </ul>
                     </div>
-                </div>
+                </div> */}
             </div>
-            <div className="resume__skills">
-                <h3>Skills</h3>
+            <div className="resume__section">
+                <h3 className="resume__section-title">Skills</h3>
                 <div className="skills-container">
                     <ul>
                         <li>HTML/CSS</li>
